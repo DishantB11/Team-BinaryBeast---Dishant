0b32acd9b1b03b8c86aed793bb6f1aea1530a47a
yhcb21 <ybhondave9@gmail.com>
2026-07-31 02:26:54 +0530

    Dashboard ui, audio player

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
 import { Sidebar } from './components/layout/Sidebar';
 import { Topbar } from './components/layout/Topbar';
 import { DashboardView } from './components/views/DashboardView';
@@ -7,6 +8,8 @@ import { TasksFocusView } from './components/views/TasksFocusView';
 import { FocusHubView } from './components/views/FocusHubView';
 import { ClassroomView } from './components/views/ClassroomView';
 import { HeatmapView } from './components/views/HeatmapView';
+import { AudioPlayer } from './components/AudioPlayer';
+import { FloatingPomodoro } from './components/FloatingPomodoro';
 
 function App() {
   const [activeView, setActiveView] = useState('dashboard');
@@ -31,21 +34,29 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-[#1e1e1e] text-white flex">
-      {/* Notion Sidebar Layout */}
-      <Sidebar activeView={activeView} setActiveView={setActiveView} />
+    <>
+      <div className="min-h-screen bg-[#1e1e1e] text-white flex">
+        {/* Notion Sidebar Layout */}
+        <Sidebar activeView={activeView} setActiveView={setActiveView} />
 
-      {/* Main Workspace Frame */}
-      <div className="flex-1 flex flex-col pl-[260px]">
-        {/* Top Header bar */}
-        <Topbar />
+        {/* Main Workspace Frame */}
+        <div className="flex-1 flex flex-col pl-[260px]">
+          {/* Top Header bar */}
+          <Topbar />
 
-        {/* Inner Content scroll layout */}
-        <main className="flex-1 mt-[60px] p-8 overflow-y-auto max-w-[1200px] w-full mx-auto pb-20">
-          {renderActiveView()}
-        </main>
+          {/* Inner Content scroll layout */}
+          <main className="flex-1 mt-[60px] p-8 overflow-y-auto max-w-[1200px] w-full mx-auto pb-20">
+            {renderActiveView()}
+          </main>
+        </div>
       </div>
-    </div>
+
+      {/* Audio Player rendered via Portal */}
+      {createPortal(<AudioPlayer />, document.body)}
+
+      {/* Floating Pomodoro Timer rendered via Portal */}
+      {createPortal(<FloatingPomodoro />, document.body)}
+    </>
   );
 }
 
